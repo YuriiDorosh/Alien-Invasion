9d7f65af16c69f5ddf1d4bbac3a3a6f090983690
yurii <yuriydorosh2005@gmail.com>
2023-01-12 01:29:58 +0200

    fixed module

--- bullet.py
+++ bullet.py
@@ -6,7 +6,8 @@ class Bullet(Sprite):
 
     def __init__(self, ai_game):
         super().__init__()
-        self.screen = ai_game.settings
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.color = self.settings.bullet_color
 
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width,

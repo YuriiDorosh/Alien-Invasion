a392b409e6f5c512b49e2cd28717af645a0d965a
yurii <yuriydorosh2005@gmail.com>
2023-01-11 23:37:45 +0200

    upgrade method "run_game" by division of the functional into two other methods

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,16 +17,20 @@ class AlienInvasion:
         self.ship = Ship(self)
 
     def run_game(self):
-
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-                self.screen.fill(self.settings.bg_color)
-                self.ship.blitme()
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-                pygame.display.flip()
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':

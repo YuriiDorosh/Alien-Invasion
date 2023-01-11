a5f0e4d35706d735bfdc79362e63615c826f72d1
yurii <yuriydorosh2005@gmail.com>
2023-01-11 22:49:48 +0200

    add background color and add indentation in line 21

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,6 +9,7 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption('Alien Invasion')
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
 
@@ -17,7 +18,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            pygame.display.flip()
+                pygame.display.flip()
 
 
 if __name__ == '__main__':

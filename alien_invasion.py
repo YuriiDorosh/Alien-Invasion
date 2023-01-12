1f8fe2cc2b1c1cf8113be7a5ec486c58f0054a4a
yurii <yuriydorosh2005@gmail.com>
2023-01-13 00:34:21 +0200

    add button in the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from time import sleep
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from button import Button
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -28,6 +29,8 @@ class AlienInvasion:
         self.aliens = pygame.sprite.Group()
 
         self._create_fleet()
+        # creating "Play" button
+        self.play_button = Button(self, "Play")
 
     def run_game(self):
         while True:
@@ -153,6 +156,9 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+        # drawing "Play" button, if game the game is inactive
+        if not self.stats.game_active:
+            self.play_button.draw_button()
 
         pygame.display.flip()
 

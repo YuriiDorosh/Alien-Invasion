6b974a1298a778235dc0c5cb8ce7a106480d4bfc
yurii <yuriydorosh2005@gmail.com>
2023-01-13 01:50:30 +0200

    fixed problem: add line number 10( self.stats = ai_game.stats )

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,7 @@ class Scoreboard:
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
+        self.stats = ai_game.stats
         # Fonts for statistic
         self.text_color = (30, 30, 30)
         self.font = pygame.font.SysFont(None, 48)

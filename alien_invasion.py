bf931b467d0a8fe2ecea2a7e528b9fc4a18fc19c
yurii <yuriydorosh2005@gmail.com>
2023-01-13 01:48:37 +0200

    add score table

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from time import sleep
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -23,6 +24,7 @@ class AlienInvasion:
         pygame.display.set_caption('Alien Invasion')
 
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -176,7 +178,9 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
-        # drawing "Play" button, if game the game is inactive
+        # Drawing information about score
+        self.sb.show_score()
+        # Drawing "Play" button, if game the game is inactive
         if not self.stats.game_active:
             self.play_button.draw_button()
 

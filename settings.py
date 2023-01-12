d5fafc634394b6a5859b2e15011ebff003e56bcc
yurii <yuriydorosh2005@gmail.com>
2023-01-12 23:25:59 +0200

    add limit of ship`s life

--- settings.py
+++ settings.py
@@ -7,6 +7,7 @@ class Settings:
         self.bg_color = (230, 230, 230)
         # ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
         # bullets settings
         self.bullet_speed = 2.5
         self.bullet_width = 3

367da13b5513c0a135ace37a5ee0f3d920a2f154
yurii <yuriydorosh2005@gmail.com>
2023-01-13 02:49:56 +0200

    increasing price of aliens

--- settings.py
+++ settings.py
@@ -21,6 +21,8 @@ class Settings:
         self.fleet_direction = 1
         # How fast the game should speed up
         self.speedup_scale = 1.1
+        # How quickly the price of aliens increases
+        self.score_scale = 1.5
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
@@ -36,3 +38,4 @@ class Settings:
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)

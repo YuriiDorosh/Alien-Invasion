5053ce3f6adb670edd78087178b202f4936f68d7
yurii <yuriydorosh2005@gmail.com>
2023-01-13 05:40:07 +0200

    add record

--- game_stats.py
+++ game_stats.py
@@ -5,6 +5,8 @@ class GameStats:
         self.reset_stats()
         # Start game in an inactive state
         self.game_active = False
+        # The record is not canceled
+        self.high_score = 0
 
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
